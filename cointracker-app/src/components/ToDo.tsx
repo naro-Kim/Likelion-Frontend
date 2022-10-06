4cbd223a73628e3ab202e4becb9ae5543858ad21
naro_Kim <gracias9022@gmail.com>
2022-10-07 03:38:11 +0900

    #5.16

--- cointracker-app/src/components/ToDo.tsx
+++ cointracker-app/src/components/ToDo.tsx
@@ -1,13 +1,39 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ title }: IToDo) {
+function ToDo({ title, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  // function to modify category
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((prev) => {
+      const targetId = prev.findIndex((toDo) => toDo.id === id); // get Index of target
+      const newToDo = { title, id, category: name as any };
+      return [...prev.slice(0, targetId), newToDo, ...prev.slice(targetId + 1)]; // function must return Updater parameter
+    });
+  };
+
   return (
     <>
       <li>
         <span>{title}</span>
-        <button>ToDo</button>
-        <button>Done</button>
-        <button>Doing</button>
+        {category !== "TODO" && (
+          <button name="TODO" onClick={onClick}>
+            To Do
+          </button>
+        )}
+        {category !== "DONE" && (
+          <button name="DONE" onClick={onClick}>
+            Done
+          </button>
+        )}
+        {category !== "DOING" && (
+          <button name="DOING" onClick={onClick}>
+            Doing
+          </button>
+        )}
       </li>
       ;
     </>
